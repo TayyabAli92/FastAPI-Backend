f553577ba15fd41e7b6be144d74f40ddafb75f4d
TayyabAli92 <hameshak110@gmail.com>
2025-12-27 22:34:19 +0500

    update-fastapi

--- api/app.py
+++ api/app.py
@@ -34,7 +34,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3001"
+        "http://localhost:3001",
+        "http://localhost:3000"
         ],  # In production, replace with specific origins
     allow_credentials=True,
     allow_methods=["*"],
--- app.py
+++ app.py
@@ -24,7 +24,8 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  # In production, replace with specific origins
+    allow_origins=["http://localhost:3001",
+                   "http://localhost:3000"],  # In production, replace with specific origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
